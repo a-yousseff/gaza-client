ea62f3d5271268dc8eed75327ad68b2e8aa4929b
a-yousseff <a.yousseff@pm.me>
2024-02-24 17:18:14 +0100

    home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'gaza-client';
+  private http: HttpClient = inject(HttpClient);
+  text: any;
+  constructor() {
+    this.http.get<string>('http://localhost:8080/', {responseType: 'text' as 'json'}).subscribe({
+      next: data => {
+        this.text = data;
+      },
+      error: error => {
+        this.text = error;
+      }
+    })
+  }
 }
